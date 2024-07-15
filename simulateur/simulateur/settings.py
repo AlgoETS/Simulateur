d73bce993e8579155304de9c05062337c92e3d66
Antoine Boucher <antoine@antoineboucher.info>
2024-07-15 19:58:45 -0400

    fix

--- simulateur/simulateur/settings.py
+++ simulateur/simulateur/settings.py
@@ -351,4 +351,10 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = False
 
+
+INFLUXDB_URL = os.getenv("INFLUXDB_URL", "http://localhost:8086")
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "my-token")
+INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "my-org")
+INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "my-bucket")
+
 CACHE_TTL = 60 * 15
