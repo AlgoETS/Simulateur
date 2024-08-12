d16930f1df479cdb2cbd113f1c3064ae5c20e04d
Antoine Boucher <antoine.bou13@gmail.com>
2024-08-12 14:48:05 -0400

    change docker-compose.yml

--- simulateur/simulation/management/commands/createsuperadmin.py
+++ simulateur/simulation/management/commands/createsuperadmin.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.management.commands import createsuperuser
 from django.core.management import CommandError
-
+from django.contrib.auth import get_user_model
 
 class Command(createsuperuser.Command):
-    help = 'Crate a superadmin user with a password non-interactively'
+    help = 'Create a superadmin user with a password non-interactively'
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
@@ -13,16 +13,29 @@ class Command(createsuperuser.Command):
         )
 
     def handle(self, *args, **options):
-        password = options.get('password')
+        User = get_user_model()
         username = options.get('username')
+        password = options.get('password')
+        email = options.get('email')
         database = options.get('database')
 
-        if password and not username:
-            raise CommandError("--username is required if specifying --password")
+        if not username:
+            raise CommandError("--username is required")
+        if not email:
+            raise CommandError("--email is required")
 
-        super(Command, self).handle(*args, **options)
-
-        if password:
-            user = self.UserModel._default_manager.db_manager(database).get(username=username)
+        user = User.objects.filter(username=username).first()
+        if user:
+            self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists.'))
+            if password:
+                user.set_password(password)
+                user.save()
+                self.stdout.write(self.style.SUCCESS(f'Password for superuser "{username}" was updated successfully.'))
+        else:
+            options['interactive'] = False
+            options['email'] = email
+            super(Command, self).handle(*args, **options)
+            user = User.objects.get(username=username)
             user.set_password(password)
             user.save()
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully.'))
