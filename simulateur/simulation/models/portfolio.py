f22b13edb018368cd8e1f38157f3e0f9b1cd907a
Olivier Giguère <olivier.giguere.2@ens.etsmtl.ca>
2024-08-12 19:01:06 -0400

    added diagrams

--- simulateur/simulation/models/portfolio.py
+++ simulateur/simulation/models/portfolio.py
@@ -1,22 +1,29 @@
 from django.db import models
 
+
 class Portfolio(models.Model):
-    owner = models.OneToOneField('UserProfile', on_delete=models.CASCADE, null=True, blank=True, related_name='portfolio')
-    teams = models.ManyToManyField('Team', related_name='portfolios', blank=True)
+    owner = models.OneToOneField(
+        "UserProfile",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="portfolio",
+    )
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    stocks = models.ManyToManyField('Stock', through='StockPortfolio')
-    transactions = models.ManyToManyField('TransactionHistory', related_name='portfolios', blank=True)
+    stocks = models.ManyToManyField("StockPortfolio", blank=True)
+    
 
     def __str__(self):
-        return f'Portfolio for {self.owner or self.team}'
+        return f"Portfolio for {self.owner or self.team}"
 
     class Meta:
         verbose_name_plural = "Portfolios"
 
+
 class StockPortfolio(models.Model):
-    stock = models.ForeignKey('Stock', on_delete=models.CASCADE)
-    portfolio = models.ForeignKey('Portfolio', on_delete=models.CASCADE)
+    stock = models.ForeignKey("Stock", on_delete=models.CASCADE,blank=True)
+    portfolio = models.ForeignKey("Portfolio", on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
 
     class Meta:
-        unique_together = ('stock', 'portfolio')
+        unique_together = ("stock", "portfolio")
